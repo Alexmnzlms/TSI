bddfa2cbf626f5a960716a535901de9fc88a65d5
Alexmnzlms <alexmanza2@gmail.com>
2020-03-19 18:03:19 +0100

    Empezando A*

--- Practica/Practica1_BusquedaHeuristica_Videojuegos/TSI-P1/src/src_manzanares_lemus_alejandro/Agent.java
+++ Practica/Practica1_BusquedaHeuristica_Videojuegos/TSI-P1/src/src_manzanares_lemus_alejandro/Agent.java
@@ -24,30 +24,68 @@ public class Agent extends AbstractPlayer {
     portal = posiciones[0].get(0).position;
     portal.x = Math.floor(portal.x / fescala.x);
     portal.y = Math.floor(portal.y / fescala.y);
+
+    System.out.println("Portal_x:");
+    System.out.println(portal.x);
+    System.out.println("Portal_y:");
+    System.out.println(portal.y);
   }
 
   public void init(StateObservation stateObs, ElapsedCpuTimer elapsedTimer){
 
   }
 
-  public ArrayList<Types.ACTIONS> A_estrella (StateObservation stateObs, ElapsedCpuTimer elapsedTimer){
+  public ArrayList<Types.ACTIONS> A_estrella (Vector2d destino, StateObservation stateObs, ElapsedCpuTimer elapsedTimer){
     ArrayList<Node> abiertos = new ArrayList<Node>();
     ArrayList<Node> cerrados = new ArrayList<Node>();
     ArrayList<Types.ACTIONS> path = new ArrayList<Types.ACTIONS>();
-    Vector2d pos_inicial = new Vector2d((stateObs.getAvatarPosition().x / fescala.x, stateObs.getAvatarPosition().y / fescala.y)
-    Nodo padre = new Nodo(pos_inicial);
+    Vector2d pos_inicial = new Vector2d(stateObs.getAvatarPosition().x / fescala.x, stateObs.getAvatarPosition().y / fescala.y);
+    Vector2d new_pos;
+    Node padre = new Node(stateObs, pos_inicial,destino,0,Types.ACTIONS.ACTION_RIGHT);
+    Node actual;
     abiertos.add(padre);
-    boolean final = false;
+    boolean fin= false;
 
-    while(!final){
+    while(!fin){
+      actual = abiertos.get(0);
+      ArrayList<Types.ACTIONS> acciones = stateObs.getAvailableActions();
+      for (int i = 0; i < acciones.size(); i++){
+        System.out.println(acciones.get(i));
+        if(acciones.get(i) != Types.ACTIONS.ACTION_USE) {
+          //Calcular aqui la nueva posicion en funcion de la accion
+          new_pos = new Vector2d(stateObs.getAvatarPosition().x / fescala.x, stateObs.getAvatarPosition().y / fescala.y);
+          if(acciones.get(i) == Types.ACTIONS.ACTION_UP){
+            new_pos.y = new_pos.y + 1;
+          } else if(acciones.get(i) == Types.ACTIONS.ACTION_DOWN){
+            new_pos.y = new_pos.y - 1;
+          } else if(acciones.get(i) == Types.ACTIONS.ACTION_RIGHT){
+            new_pos.x = new_pos.x + 1;
+          } else if(acciones.get(i) == Types.ACTIONS.ACTION_LEFT){
+            new_pos.x = new_pos.x - 1;
+          }
 
+          if(acciones.get(i) == actual.getAccion()) {
+            System.out.println("misma accion");
+            cerrados.add(new Node(stateObs, new_pos, destino, actual.getCoste_camino()+1.0,acciones.get(i)));
+          } else {
+            System.out.println("distinta accion");
+            cerrados.add(new Node(stateObs, new_pos, destino, actual.getCoste_camino()+2.0,acciones.get(i)));
+          }
+        }
+      }
+      for(int i = 0; i < cerrados.size(); i++) {
+        System.out.println("Accion del nodo:");
+        System.out.println(cerrados.get(i).getAccion());
+        System.out.println("Coste del nodo:");
+        System.out.println(cerrados.get(i).getCoste_total());
+      }
+      fin = true;
     }
     return path;
   }
 
   public Types.ACTIONS act( StateObservation stateObs, ElapsedCpuTimer elapsedTimer){
-
-    A_estrella(stateObs,elapsedTimer);
+    A_estrella(portal,stateObs,elapsedTimer);
     return Types.ACTIONS.ACTION_NIL;
 
     /*Vector2d avatar =  new Vector2d(stateObs.getAvatarPosition().x / fescala.x,
--- Practica/Practica1_BusquedaHeuristica_Videojuegos/TSI-P1/src/src_manzanares_lemus_alejandro/Node.java
+++ Practica/Practica1_BusquedaHeuristica_Videojuegos/TSI-P1/src/src_manzanares_lemus_alejandro/Node.java
@@ -1,8 +1,12 @@
 package src_manzanares_lemus_alejandro;
 
+import core.game.Observation;
+import core.game.StateObservation;
 import ontology.Types;
 import tools.Direction;
 import tools.Vector2d;
+
+import java.util.ArrayList;
 
 public class Node{
 
@@ -10,15 +14,28 @@ public class Node{
     private double coste_camino;
     private double coste_total;
     private Types.ACTIONS accion;
-    private Vector2d position;
+    private Vector2d posicion;
+    private Vector2d portal;
+    private StateObservation stateObs;
 
-    public Node(Vector2d pos, int coste,Types.ACTIONS accion)
+    public Node(StateObservation stateObs, Vector2d pos, Vector2d port, double coste, Types.ACTIONS accion)
     {
-        position = pos;
+        posicion = pos;
+        portal = port;
         coste_camino = coste;
         distancia_objetivo = calcular_heuristica();
         coste_total = distancia_objetivo + coste_camino;
         this.accion = accion;
+        this.stateObs = stateObs;
+        System.out.println("Pos_x:");
+        System.out.println(posicion.x);
+        System.out.println("Pos_y:");
+        System.out.println(posicion.y);
+
+        System.out.println("Coste camino:");
+        System.out.println(coste_camino);
+        System.out.println("Distancia objetivo:");
+        System.out.println(distancia_objetivo);
     }
 
     public int compareTo(Node n) {
@@ -29,9 +46,24 @@ public class Node{
         return 0;
     }
 
-    public boolean equals(Object o)
-    {
-        return this.position.equals(((Node)o).position);
+    private double calcular_heuristica(){
+        return Math.abs(posicion.x - portal.x) + Math.abs(posicion.y - portal.y);
     }
 
+    public boolean equals(Node o)
+    {
+        return this.posicion.equals(o.posicion) && this.accion != o.accion;
+    }
+
+    public double getCoste_total() {
+        return coste_total;
+    }
+
+    public Types.ACTIONS getAccion() {
+        return accion;
+    }
+
+    public double getCoste_camino() {
+        return coste_camino;
+    }
 }
