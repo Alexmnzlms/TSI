5d35baa8acfcb30598768ace191e69847a18590b
Alexmnzlms <alexmanza2@gmail.com>
2020-03-31 14:33:19 +0200

    Memoria terminada

--- Practica/Practica1_BusquedaHeuristica_Videojuegos/generar_entrega.sh
+++ Practica/Practica1_BusquedaHeuristica_Videojuegos/generar_entrega.sh
@@ -4,6 +4,6 @@ cp doc/demo.pdf .
 mv demo.pdf Memoria_Practica1_Alejandro_Manzanares_Lemus.pdf
 mkdir src_manzanares_lemus_alejandro
 cp TSI-P1/src/src_manzanares_lemus_alejandro/* ./src_manzanares_lemus_alejandro
-zip -r practica1_manzanares_lemus_alejandro.zip ./src_manzanares_lemus_alejandro ./Memoria_Practica1_Alejandro_Manzanares_Lemus.pdf
+zip -r practica1_anzanares_lemus_alejandro.zip ./src_manzanares_lemus_alejandro ./Memoria_Practica1_Alejandro_Manzanares_Lemus.pdf
 rm -rf src_manzanares_lemus_alejandro/
 rm Memoria_Practica1_Alejandro_Manzanares_Lemus.pdf
